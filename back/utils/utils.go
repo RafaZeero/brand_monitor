fbe67c9cbf8639505ddb2562c380620a9054fd33
RafaZeero <rafaa99@gmail.com>
2024-07-12 21:55:13 -0300

    feat: add more utilities funcs

--- back/utils/utils.go
+++ back/utils/utils.go
@@ -2,10 +2,14 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func RespondWithJSON(ctx *gin.Context, code int, payload interface{}) {
@@ -32,4 +36,47 @@ func RespondWithError(ctx *gin.Context, code int, msg string) {
 	}
 
 	RespondWithJSON(ctx, code, errResponse{Error: msg, Success: false})
+}
+
+func EnvOrFatal(key string) string {
+	// get root dir
+	rootDir := getRootDir()
+
+	envPath := filepath.Join(rootDir, "/back/.env")
+
+	// load .env file
+	if err := godotenv.Load(envPath); err != nil {
+		panic(fmt.Sprintf("Error loading .env file - path: %s", envPath))
+	}
+
+	value := os.Getenv(key)
+	if value == "" {
+		panic("missing required environment variable " + key)
+	}
+
+	return value
+}
+
+func getRootDir() string {
+	// get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	// search for the root directory by looking for the .git folder
+	for {
+		if _, err := os.Stat(filepath.Join(cwd, ".git")); err == nil {
+			// found the .git folder, return this directory as the root
+			return cwd
+		}
+
+		// move to the parent directory
+		parent := filepath.Dir(cwd)
+		if parent == cwd {
+			// reached the root without finding .git folder, return current directory
+			return cwd
+		}
+		cwd = parent
+	}
 }
