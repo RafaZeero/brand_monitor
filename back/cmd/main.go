6d7cbd94725815293dd5750bf98864c0fef46dac
RafaZeero <rafaa99@gmail.com>
2024-07-16 08:17:50 -0300

    feat(back): add ping to check connection on startup

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/RafaZeero/brand_monitor/cmd/api"
+	"github.com/RafaZeero/brand_monitor/db"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
-	server := api.NewApiServer("3333")
+	client, err := db.NewDbConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(client)
+
+	server := api.NewApiServer("3333", client)
+
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *mongo.Client) {
+	if db.Ping(context.TODO(), nil) != nil {
+		log.Fatal("failed to connect to database")
+	}
+
+	log.Println("connected to database")
 }
