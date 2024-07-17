485a4d08ae7dca01b3e45f9452c61f44cb1efbbe
RafaZeero <rafaa99@gmail.com>
2024-07-16 22:31:03 -0300

    fix: docker compose file

--- back/db/db.go
+++ back/db/db.go
@@ -12,7 +12,7 @@ func NewDbConnection() (*mongo.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:27017"))
 	if err != nil {
 		return nil, err
 	}
